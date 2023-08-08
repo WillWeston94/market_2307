ac74ce0eefa7717011cc31a253749be56615d4b6
Will Weston <Williamweston81@gmail.com>
2023-08-08 10:17:11 -0500

    Weston(1) Create: Vendor class with nil hash - pass

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -17,5 +17,4 @@ RSpec.describe Item do
     expect(@item2.name).to eq("Tomato")
     expect(@item2.price).to eq(0.5)
   end
-  
 end
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,15 @@
+require './lib/item'
+require './lib/vendor'
+require 'pry'
+
+RSpec.describe Vendor do
+  before(:each) do
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  it 'Vendor exists with no inventory' do
+    expect(@vendor).to be_an_instance_of(Vendor)
+    expect(@vendor.name).to eq("Rocky Mountain Fresh")
+    expect(@vendor.inventory).to eq({})
+  end
+end
